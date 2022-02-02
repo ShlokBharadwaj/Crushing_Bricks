206f37825539af5bb3fb88f87e4adc916bab00e1
Shlok <shlokbharadwaj7@gmail.com>
2022-02-02 23:40:50 +0530

    added frame for game

--- src/CB/Main.java
+++ src/CB/Main.java
@@ -1,10 +1,18 @@
 package CB;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		JFrame frame = new JFrame();
+		//GamePlay gamePlay = new GamePlay();
+		frame.setBounds(10,10,700,600);
+		frame.setTitle("Crushing Bricks");
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
