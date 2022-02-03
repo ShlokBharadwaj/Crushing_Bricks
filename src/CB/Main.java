c94a66c4f96a516593dd31fc666ef07109e54d41
Shlok <shlokbharadwaj7@gmail.com>
2022-02-04 00:04:33 +0530

    added new code

--- src/CB/Main.java
+++ src/CB/Main.java
@@ -7,12 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JFrame frame = new JFrame();
-		//GamePlay gamePlay = new GamePlay();
+		GamePlay gamePlay = new GamePlay();
 		frame.setBounds(10,10,700,600);
 		frame.setTitle("Crushing Bricks");
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(gamePlay);
 	}
 
 }
